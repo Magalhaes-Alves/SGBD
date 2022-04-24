72caa23b05ce5b9fd4a9edfb81921e177b0625a7
thiagogadelhaa <thiagogadelhaa1@gmail.com>
2022-04-24 19:27:12 -0300

    Carregar o arquivo de diretorio para a memoria principal,e,casonão exista, setar valores padrão

--- Tarefa 1/src/Main.py
+++ Tarefa 1/src/Main.py
@@ -1,15 +1,41 @@
+import shutil
+import funcoes
 
 
 
-
-with open("in.txt",'r') as entrada, open("out.txt",'w') as saida, open("diretorio.txt","r") as diretorio:
+with open("in.txt",'r') as entrada, open("out.txt",'w') as saida:
     comandos = entrada.readlines()
+    
+    #Seção para carregar o diretorio para a memoria principal
+    try:
+        #Tenta abrir o arquivo
+        diretorio = open("diretorio.txt","r")
+        
+        profundidadeGlobal =int(diretorio.readline()[:-1])
+        
+        diretorioEntradas = []
+        eof = False
+        
+        #Carregando os enderecos para os buckets.Nesta aplicacao, serao os nomes dos arquivos
+        while(not (eof)):
+            linha = diretorio.readline()
+            if linha=='':
+                eof=True
+            else:
+                diretorioEntradas.append(linha[:-1])
+    except OSError:
+        #Caso não exista arquivo de diretorio, são colocados os valores iniciais estabelecidos
+        profundidadeGlobal = 2
+        diretorioEntradas = ['00','01','10','11']
 
     for op in comandos:
         operacao = op
         operacao.split(':')
         if operacao[0][0:3]== "INC":
-            #Fazer inclusão
+            chave = int(operacao[1])
+            funcoes.mascara(chave,profundidadeGlobal)
+            funcoes.inserir(80, chave, diretorioEntradas[funcoes.mascara(chave, profundidadeGlobal)]+'.txt')
+            
             pass
         elif operacao[0][0:3]== "REM":
             #Fazer remoção
@@ -19,5 +45,3 @@ with open("in.txt",'r') as entrada, open("out.txt",'w') as saida, open("diretori
             pass
         else:
             pass
-        
-    
