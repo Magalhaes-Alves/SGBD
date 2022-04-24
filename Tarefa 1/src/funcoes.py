001a6df387f0e6fb960478d7ba307b1e56eedfca
thiagogadelhaa <thiagogadelhaa1@gmail.com>
2022-04-24 14:20:55 -0300

    Alterando a funcao de inserir para alterar a quantidade de entradas no bucket

--- Tarefa 1/src/funcoes.py
+++ Tarefa 1/src/funcoes.py
@@ -3,15 +3,16 @@
 Mascara de bits
 Retorna os exatos b ultimos bits de um inteiro
 """ 
-import readline
-
+import readline, shutil, tempfile
 
 def mascara(inteiro, b):
     mask = 2**(b)-1
     return inteiro & mask
 
+
 def funcaoHashing(chave_busca):
     return bin(chave_busca)
+
 
 def buscarBucket(chave_busca,bucket):
     dados_bucket = []
@@ -28,6 +29,7 @@ def buscarBucket(chave_busca,bucket):
         
     return dados_bucket
 
+
 def enderecoBucket(chave,pg):
   endereco_bucket = bin(mascara(chave,pg))[2:]
 
@@ -36,20 +38,38 @@ def enderecoBucket(chave,pg):
     endereco_bucket = '0' + endereco_bucket
   
   return endereco_bucket
+
+
 def busca(chave, diretorio, pg):
     endereco_bucket = enderecoBucket(chave,pg)
   
     return  buscarBucket(chave, endereco_bucket+'.txt')
   
+
 def inserir(id_vinho,chave_busca,bucket):
   with open(bucket,'r') as dados:
     pl_reg =dados.readline() 
     pl_reg= pl_reg.split(',') #[0] = pl ; [1] = qntd registro     
-    print(pl_reg)
+    
     if(int(pl_reg[1][:-1])<32):
-      with open(bucket, 'a') as arq:
-        arq.write(f"{id_vinho},{chave_busca}\n")
-    else:
-      pass    
+        #Passo para adicionar em um a quantidade de registros no conteudo da linha
+        quantidade_registros = list(pl_reg[1].strip())
+        quantidade_registros[0] = str (int(quantidade_registros[0]) +1)
+        pl_reg[1] = "".join(quantidade_registros)
 
+        with tempfile.NamedTemporaryFile('w', delete=False) as out:
+            out.write(f"{pl_reg[0]},{pl_reg[1]}\n")
+            eof = False
+        
+            while(not (eof)):
+                pl_reg = dados.readline()
+                if pl_reg=='':
+                    eof=True
+                else:
+                    out.write(pl_reg)
+            out.write(f"{id_vinho},{chave_busca}\n")
+        shutil.move(out.name, bucket)
+
+    else:
+        pass    
 
