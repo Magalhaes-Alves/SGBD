ac82fe52cff9aa3f27d37f2b5b419a2aa4a38f84
thiagogadelhaa <thiagogadelhaa1@gmail.com>
2022-04-24 13:32:31 -0300

    Alterações que fizemos no github

--- Tarefa 1/src/funcoes.py
+++ Tarefa 1/src/funcoes.py
@@ -14,21 +14,42 @@ def funcaoHashing(chave_busca):
     return bin(chave_busca)
 
 def buscarBucket(chave_busca,bucket):
-    
+    dados_bucket = []
     with open(bucket,'r') as dados:
         dados.readline()
-        flag =False
         eof = False
-        while(not(flag or eof)):
+        
+        while(not (eof)):
             linha = dados.readline()
             if linha=='':
                 eof=True
-            else:
-                if int(linha.split(',')[2])== chave_busca:
-                    flag =True
-        return (flag,linha)
+            elif int(linha.split(',')[1])== chave_busca:
+              dados_bucket.append(linha[:-1])
+        
+    return dados_bucket
 
-def busca(chave, diretorio):
-    pass
+def enderecoBucket(chave,pg):
+  endereco_bucket = bin(mascara(chave,pg))[2:]
+
+  
+  if len(endereco_bucket)<pg:
+    endereco_bucket = '0' + endereco_bucket
+  
+  return endereco_bucket
+def busca(chave, diretorio, pg):
+    endereco_bucket = enderecoBucket(chave,pg)
+  
+    return  buscarBucket(chave, endereco_bucket+'.txt')
+  
+def inserir(id_vinho,chave_busca,bucket):
+  with open(bucket,'r') as dados:
+    pl_reg =dados.readline() 
+    pl_reg= pl_reg.split(',') #[0] = pl ; [1] = qntd registro     
+    print(pl_reg)
+    if(int(pl_reg[1][:-1])<32):
+      with open(bucket, 'a') as arq:
+        arq.write(f"{id_vinho},{chave_busca}\n")
+    else:
+      pass    
 
 
