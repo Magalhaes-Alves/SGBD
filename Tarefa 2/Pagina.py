9e7ce674454f2cbb61ba1c6099cee98fdc5b435b
Gabriel Magalhães <magalhaesgabriel124@gmail.com>
2022-05-17 09:01:52 -0300

    Estrutura da pagina

--- Tarefa 2/Pagina.py
+++ Tarefa 2/Pagina.py
@@ -1,5 +1,29 @@
+import Tupla
+
+"""
+    Esquema das páginas
+    
+    qntd_de_tuplas: int
+    Um registro em cada linha    
+"""
+
 class Pagina():
-    def __init__(self):
+    def __init__(self,nome_arquivo):
         self._tuplas = []
         #Lista de tuplas com 12, no máximo
         self._qtd_tuplas_ocup = 0
+        self._nome_página = nome_arquivo
+
+    def adicionar_tupla(self,tupla):
+        if (self._qtd_tuplas_ocup <12):            
+            for tup in tupla.recuperar_tupla():
+            self._qtd_tuplas_ocup+=1
+        else:
+            print("Essa página está cheia.")
+
+"""     def gravar(self):
+        with open(self._nome_página+".txt","w") as saida:
+            saida.write(str(self._qtd_tuplas_ocup) + "\n")
+            for i in self._tuplas:
+                saida.write(i+"\n") """
+    
--- Tarefa 2/Tupla.py
+++ Tarefa 2/Tupla.py
@@ -2,3 +2,9 @@ class Tupla():
     def __init__(self):
         self._cols = []
         #Lista de Strings de tamanho qtd_colunas
+    
+    def adicionar_atributo(self, atrib):
+        self._cols.append(atrib)
+    
+    def recuperar_tupla(self):
+        return self._cols
