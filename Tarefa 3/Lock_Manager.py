8669ec67376d18791361fa76174f8ce5571304ca
thiagogadelhaa <thiagogadelhaa1@gmail.com>
2022-06-19 11:26:19 -0300

    Estrutura finalizada(falta testar mais), waitDie feito.Falta WoundWait e a saída solicitada

--- Tarefa 3/Lock_Manager.py
+++ Tarefa 3/Lock_Manager.py
@@ -133,16 +133,23 @@ class Lock_Manager():
 
             if trans_Adicionada.Ts < aux_Trans.Ts:
                 countRoll = 1   
-            count +=1
+            count += 1
             aux_Lock = aux_Lock.prox
             
         if countRoll>0:
             print("ROLLBACK")
 
-        if trans_Adicionada.Ts == trans_Enfileirada.Ts and count == 1 :
+        if trans_Adicionada.Ts == trans_Enfileirada.Ts and count == 1:
+            #Isto significa que a operação que ocasionou a execução do waitDie é a mesma
+            #do único bloqueio ques está na LockTable.Sendo assim, este novo Bloqueio é adicionado
+            #à LockTable
             self.Lock_Table[item].modo = Novo_Lock_Request.modo
+            self.Lock_Table[item].prox = Novo_Lock_Request
         else:
             #Coloca na Lista de Espera
+            self.tr_manager.waitForDataList[TR][0] = True
+            self.tr_manager.waitForDataList[TR].append(item)
+            #Muda este valor para informar que há operações em espera
             if item in self.WaitQ.keys():
                 
                 #Coloca no final da lista de espera associada a este item
@@ -168,26 +175,8 @@ class Lock_Manager():
         Novo_Lock_Request = Lock_Request("S", trans.Id,None, item)
         
         #Será que a transação em questão está com uma operação anterior em espera?
-        chaves2 = self.WaitQ.keys()
-        for chave in chaves2:
-            Lock_aux = self.WaitQ[chave]
-
-            while(Lock_aux!=None):
-                if(Lock_aux.TR_Id == trans.Id):
-                    #Há uma operação da mesma transação em espera, então ela deve entrar em espera
-                    if item in chaves2:
-                        Lock_aux = self.WaitQ[item]
-                        while(Lock_aux.prox!=None):
-                            Lock_aux = Lock_aux.prox
-                        Lock_aux.prox = Novo_Lock_Request
-                    else:
-                        self.WaitQ[item] = Novo_Lock_Request
-                    
-                    self.Escrever_Lock_Table()
-                    return
-
-                Lock_aux =Lock_aux.prox
-
+        if self.tr_manager.waitForDataList[TR][0] == True:
+            self.tr_manager.waitForDataList[TR].append(item)
 
         if (not item in chaves):
             #Significa que não há Pedido de bloqueio para este item
@@ -207,32 +196,15 @@ class Lock_Manager():
 
     def LX(self, TR, item):
         #Insere um bloqueio no modo exclusivo na Lock_Table
-
         self.Carregar_Lock_Table()
+
         trans = self.tr_manager.get_TR(TR)
         chaves = self.Lock_Table.keys()
-
         Novo_Lock_Request = Lock_Request("X", trans.Id,None, item)
 
-        chaves2 = self.WaitQ.keys()
-        for chave in chaves2:
-            Lock_aux = self.WaitQ[chave]
-
-            while(Lock_aux!=None):
-                if(Lock_aux.TR_Id == trans.Id):
-                    #Há uma operação da mesma transação em espera, então ela deve entrar em espera
-                    if item in chaves2:
-                        Lock_aux = self.WaitQ[item]
-                        while(Lock_aux.prox!=None):
-                            Lock_aux = Lock_aux.prox
-                        Lock_aux.prox = Novo_Lock_Request
-                    else:
-                        self.WaitQ[item] = Novo_Lock_Request
-                    
-                    self.Escrever_Lock_Table()
-                    return
-
-                Lock_aux =Lock_aux.prox
+        #Será que a transação em questão está com uma operação anterior em espera?
+        if self.tr_manager.waitForDataList[TR][0] == True:
+            self.tr_manager.waitForDataList[TR].append(item)
 
         if (not item in chaves):
             #Significa que não há Pedido de bloqueio para este item
@@ -245,7 +217,7 @@ class Lock_Manager():
         
         self.Escrever_Lock_Table()
 
-    def U(self, TR, item):
+    def U(self, TR, item,apagar):
         """Apaga os bloqueios da transacao Tr em relação a determinado item da Lock_Table"""
 
         trans = self.tr_manager.get_TR(TR)
@@ -257,51 +229,158 @@ class Lock_Manager():
         while (Corrente != None):
             Seguinte = Corrente.prox
             if trans.Id == Corrente.TR_Id:
+                print("TESTANDO ESSA BUCETA")
+                self.Printar_Lock_Table()
                 if Anterior == None:
-                    Corrente = Corrente.prox
+                    self.Lock_Table[item] = Corrente.prox
+                    Anterior = None
                 elif Seguinte == None:
                     Anterior.prox = None
+                    Anterior = Corrente
                 else:
                     Anterior.prox = Corrente.prox
+                    Anterior = Corrente
                 saida = True
-            Anterior = Corrente
             Corrente = Corrente.prox
 
         
         if(self.Lock_Table[item]==None):
-            self.Lock_Table.pop(item)
+            apagar.append(item)
 
         
         return saida
 
-    def UWaitQ(self, item):
+    def liberar_itens_LockTable(self):
         pass
+    
 
-    def apagarTr(self, TR_Id):
-        """Apaga as Transações de Tr da LockTable e verifica se operações de outras transações podem prosseguir"""
-        self.Carregar_Lock_Table()
-        print("TESTE DE CORNO1:")
-        self.Printar_Lock_Table()
-        TR = self.tr_manager.buscar_TR_por_Id(TR_Id)
-        chaves = self.Lock_Table.keys()
-        itens_Liberados = []
-
-        for chave in chaves:
-            if(self.U(TR, chave)):
-                itens_Liberados.append(chave)
-            
-
-        chaves = self.Lock_Table.keys()
+    def ajustar_WaitforData(self):
+        #Função para passar por todas as Listas de esperas das transações após a liberação de itens da WaitQ
+        #para verificar se os itens na WaitForDataLista são de operações que estão na LockTable e apenas estavam em espera
+        chaves = self.tr_manager.waitForDataList.keys()
         chaves2 = self.WaitQ.keys()
-        
+        for chave in chaves:
+            listaTR = self.tr_manager.waitForDataList[chave]
+            controle = 0
+            #Variável para contar quantas operações na WaitForDataList estão em espera por bloqueio de outra Transação
+            for item in listaTR[1:]:
+                if item in chaves2:
+                    #Se há Lista de espera associada ao item, verifica se a transação em questão tem operações nessa waitQ
+                    # que estão em espera devido a um bloqueio na lockTable
+                    TR_Id_aux = self.tr_manager.get_TR(chave).Id
+                    WaitQ_aux = self.WaitQ[item]
+                    while(WaitQ_aux!=None):
+                        if(WaitQ_aux.TR_Id == TR_Id_aux):
+                            controle +=1
+                        WaitQ_aux = WaitQ_aux.prox
+            if(controle==0):
+                self.tr_manager.waitForDataList[chave] = [False]
+                
+                
+
+    def liberar_itens_WaitQ(self, itens_Liberados,apagar):
         for item_liberado in itens_Liberados:
-            #Verifica se a WaitQ tem Transacoes associadas ao item que foi liberado
+        #Verifica se a WaitQ tem Transacoes associadas ao item que foi liberado
             chaves2 = self.WaitQ.keys()
             if item_liberado in chaves2:
                 #Se há transações associadas a esse item na lista de Espera.O primeiro bloqueio
                 #da lista de espera é pego
-                Lock_aux = self.WaitQ[item_liberado]
+                Corrente = self.WaitQ[item_liberado]
+                TR_Id_Aux = Corrente.TR_Id
+                #É pego o TR_Id do primeiro item para pegar todos os bloqueios da WaitQ com este bloqueio
+                Anterior = None
+                while (Corrente != None):
+                    Seguinte = Corrente.prox
+                    if TR_Id_Aux == Corrente.TR_Id:
+                        print(f"TR_ID:{TR_Id_Aux},Corrente.TR_Id:{Corrente.TR_Id}")
+                        aux_Excluir = Corrente
+                        #Apagar o Bloqueio em questão da WaitQ   
+                        if Anterior == None:
+                            print("TESTE 1")
+                            self.WaitQ[item_liberado] = Corrente.prox
+                            Anterior = None
+                        elif Seguinte == None:
+                            print("TESTE 2")
+                            Anterior.prox = None
+                            Anterior = Corrente
+                        else:
+                            print("TESTE 3")
+                            Anterior.prox = Corrente.prox
+                            Anterior = Corrente
+                        #Mover bloqueio excluido para a LockTable
+                        aux_Excluir.prox = None
+                        chaves = self.Lock_Table.keys()
+                        if item_liberado not in chaves:
+                            self.Lock_Table[item_liberado] = aux_Excluir
+                        else:
+                            LockTable_aux = self.Lock_Table[item_liberado]
+                            while(LockTable_aux.prox!=None):
+                                LockTable_aux = LockTable_aux.prox
+                            LockTable_aux.prox = aux_Excluir
+
+                        #Excluir itens liberados da WaitForDataList
+                        TR = self.tr_manager.buscar_TR_por_Id(TR_Id_Aux)
+                        self.tr_manager.waitForDataList[TR].remove(item_liberado)
+
+                    Corrente = Corrente.prox
+                if(self.WaitQ[item_liberado]==None):
+                    apagar.append(item_liberado)
+
+    def apagarTr(self, TR_Id):
+        """Apaga as Transações de Tr da LockTable e verifica se operações de outras transações podem prosseguir"""
+        self.Carregar_Lock_Table()
+
+        TR = self.tr_manager.buscar_TR_por_Id(TR_Id)
+        chaves = self.Lock_Table.keys()
+        itens_Liberados = []
+        apagar = []
+        for chave in chaves:
+            if(self.U(TR, chave,apagar)):
+                itens_Liberados.append(chave)
+        for apagaratual in apagar:
+            self.Lock_Table.pop(apagaratual)
+        apagar = []
         
+        self.liberar_itens_WaitQ(itens_Liberados,apagar)
+        for apagaratual in apagar:
+            self.WaitQ.pop(apagaratual)
+        apagar = [] 
+        self.ajustar_WaitforData()
+        
+        commitado = len(self.CommitsEmEspera)
+        #Variável para entrar no loop a seguir e saber se alguma Transação foi commitada
+
+        while(commitado>0):
+            for commitAtual in self.CommitsEmEspera:
+                TR_Aux = self.tr_manager.buscar_TR_por_Id(commitAtual)
+                if self.tr_manager.waitForDataList[TR_Aux][0] == False:
+                    
+                    chaves = self.Lock_Table.keys()
+                    itens_Liberados = []
+                    apagar = []
+                    for chave in chaves:
+                        if(self.U(TR_Aux, chave,apagar)):
+                            itens_Liberados.append(chave)
+                    for apagaratual in apagar:
+                        self.Lock_Table.pop(apagaratual)
+                    apagar = []    
+                    
+
+                    self.liberar_itens_WaitQ(itens_Liberados,apagar)
+                    for apagaratual in apagar:
+                        self.WaitQ.pop(apagaratual)
+                    apagar = [] 
+                    self.ajustar_WaitforData()
+
+                    self.CommitsEmEspera.remove(commitAtual)
+                    #Como foi commitado, o commit relativo à transação em questão é removido
+                    commitado += len(self.CommitsEmEspera)
+                    #Isto é feito para que seja analisado se dá para commitar mais alguma transação depois da liberação desta
+                commitado -=1
+        
+        
+        print("WaitQ")
+        self.printar_WaitQ()
         self.Escrever_Lock_Table()
         
 
@@ -362,8 +441,8 @@ class Lock_Manager():
             if(not self.serializavel):
                 print("Esta história não é serializável")
                 break
-        self.printar_WaitQ()
+
         print(f"""
-        {self.CommitsEmEspera}
+        {type(self.CommitsEmEspera)}
         """)
 
