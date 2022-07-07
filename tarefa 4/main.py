f79223a55ded9dfb8e00053431c13d1d0d323c16
thiagogadelhaa <thiagogadelhaa1@gmail.com>
2022-07-07 13:31:16 -0300

    Adicionando os comentários e a saída

--- tarefa 4/main.py
+++ tarefa 4/main.py
@@ -1,6 +1,7 @@
 from Log import Log
 from Recuperacao import Recuperacao
 
-
+#Instancia a classe em que o algoritmo de recuperação será executado e passa
+#como parametro o nome do arquivo de entrada.
 algoritmo = Recuperacao('in2.txt')
 
