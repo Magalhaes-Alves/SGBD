ec5ff5bcf43605c42a5542cc72e81f4a9cc5aae5
Gabriel Magalhães <magalhaesgabriel124@gmail.com>
2022-07-07 13:42:41 -0300

    Entrada de dados consertada

--- tarefa 4/Log.py
+++ tarefa 4/Log.py
@@ -7,7 +7,7 @@ class Log:
         self._log= []
         with open(arquivo, 'r') as historias:
             self._log = historias.readlines()
-            self._log = list(map(lambda x: x[2:-3].split(' | '),self._log))
+            self._log = list(map(lambda x: x[2:-2].split('|'),self._log))
 
     def mostrarLog(self):
         for i in range(len(self._log)):
